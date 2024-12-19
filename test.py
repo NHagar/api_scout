1295c8208a0b4642cfa694b6dd45ba133f653b38
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2024-12-18 18:45:37 -0600

    llm bug fixes

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 # https://inspectelement.org/apis.html
-# test w/LLM
 # integrate LLM workflow
 # prototype interaction model
 # prototype UI
@@ -18,14 +17,28 @@ def parse_logfile(logfile):
     items_formatted = []
 
     for item in items:
+        request_url = item["request"]["url"]
+        try:
+            response_content = item["response"]["content"]["text"]
+        except KeyError:
+            response_content = None
+
+        try:
+            response_type = item["response"]["content"]["mimeType"]
+        except KeyError:
+            response_type = None
+
+        request_headers = item["request"]["headers"]
+        response_headers = item["response"]["headers"]
+
         item_formatted = f"""---------------------
-        Request URL: {item["request"]["url"]}
+        Request URL: {request_url}
 
-        Response Content: {item["response"]["content"]["text"]}
-        Response Type: {item["response"]["content"]["mimeType"]}
+        Response Content: {response_content}
+        Response Type: {response_type}
 
-        Request Headers: {item["request"]["headers"]}
-        Response Headers: {item["response"]["headers"]}
+        Request Headers: {request_headers}
+        Response Headers: {response_headers}
         ---------------------
         """
         items_formatted.append(item_formatted)
@@ -47,7 +60,7 @@ def process_item(item):
 
     resp = client.chat.completions.create(model="gemma-2-9b-it", messages=messages)
 
-    decision = resp.choices[0].message.content
+    decision = resp.choices[0].message.content.strip().lower()
 
     if decision == "yes":
         messages = [
@@ -60,5 +73,13 @@ def process_item(item):
         document = resp.choices[0].message.content
 
         return document
-    else:
+    elif decision == "no":
         return None
+    else:
+        print("Invalid decision")
+        print(decision)
+
+
+items = parse_logfile("www.nytimes.com.har")
+test = process_item(items[0])
+print(test)
