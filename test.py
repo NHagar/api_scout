f031d6bf364bf527463e9d67a7ad31ecc965a8bc
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2024-12-18 18:24:46 -0600

    parsing function

--- test.py
+++ test.py
@@ -4,3 +4,27 @@
 # integrate LLM workflow
 # prototype interaction model
 # prototype UI
+import json
+
+
+def parse_logfile(logfile):
+    with open(logfile, "r") as f:
+        logs = json.load(f)
+
+    items = logs["log"]["entries"]
+    items_formatted = []
+
+    for item in items:
+        item_formatted = f"""---------------------
+        Request URL: {item["request"]["url"]}
+
+        Response Content: {item["response"]["content"]["text"]}
+        Response Type: {item["response"]["content"]["mimeType"]}
+
+        Request Headers: {item["request"]["headers"]}
+        Response Headers: {item["response"]["headers"]}
+        ---------------------
+        """
+        items_formatted.append(item_formatted)
+
+    return items_formatted
